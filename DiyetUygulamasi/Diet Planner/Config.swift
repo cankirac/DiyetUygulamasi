2ed335e5920ab460e51f14943ed4820cd7d2f373
cankirac <154017812+cankirac@users.noreply.github.com>
2024-04-26 12:59:38 +0300

    Update Config.swift

--- DiyetUygulamasi/Diet Planner/Config.swift
+++ DiyetUygulamasi/Diet Planner/Config.swift
@@ -6,13 +6,13 @@ var STATUS = 1
 var planCategories = ["Kilo Verme", "Kan Basıncı", "Gluttensiz", "Sütsüz", "Şeker", "Temiz Beslenme", "Vejetaryen", "Diğer"]
 
 var kiloVerme = ["Aylık Kilo Verme", "Haftalık Kilo Verme"]
-var kanBasıncı = ["High Blood Pressure", "1200 Calories BP", "1500 Calories BP", "2000 Calories", "7 Day Blood Pressure"]
+var kanBasinci = ["Kan Basıncı", "1200 Kalori BP", "1500 Kalori BP", "2000 Kalori", "7 Günlük Kan Basıncı"]
 var gluttensiz = ["7 Gün Gluttensiz", "Gluttensiz"]
-var sütsüz = ["Süt Ürünleri İçermez", "Sağlıklı Gebelik", "1200 Kalori", "1500 Kalori"]
-var şekersiz = ["Diyabet", "Şeker-Detoks", "Şekersiz"]
+var sutsuz = ["Süt Ürünleri İçermez", "Sağlıklı Gebelik", "1200 Kalori", "1500 Kalori"]
+var sekersiz = ["Diyabet", "Şeker-Detoks", "Şekersiz"]
 var temizBeslenme = ["Başlangıç", "İleri Düzey", "Orta"]
 var vejetaryen = ["7 Günde Kilo Verme", "7 Günde Sağlıklı"]
-var diğerleri = ["7 Gün Akdeniz", "Eleme"]
+var digerleri = ["7 Gün Akdeniz", "Eleme"]
 
 func getJSONFileName(categoryIndex: Int, subCategoryIndex: Int) -> String {
     var jsonFile = ""
